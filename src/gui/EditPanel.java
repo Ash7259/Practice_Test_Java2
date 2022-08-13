d9badf537c66718971fa44c8cf35f15a9f709553
Ash7259 <110978110+Ash7259@users.noreply.github.com>
2022-08-13 08:49:10 +0800

    Update EditPanel.java
    Prettify

--- src/gui/EditPanel.java
+++ src/gui/EditPanel.java
@@ -15,71 +15,72 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class EditPanel extends JPanel{
+public class EditPanel extends JPanel {
 	private MainFrame main;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private Records rec;
 	private int index;
+
 	public EditPanel(MainFrame main, int ind, Records rec) {
 		this.main = main;
 		this.setSize(347, 312);
 		setLayout(null);
-		this.index=ind;
+		this.index = ind;
 		this.rec = rec;
-		
+
 		JLabel lblEditScreen = new JLabel("Edit Panel");
 		lblEditScreen.setFont(new Font("Tahoma", Font.BOLD, 16));
 		lblEditScreen.setBounds(92, 16, 118, 20);
 		add(lblEditScreen);
-		
+
 		textField = new JTextField();
 		textField.setBounds(102, 52, 146, 26);
 		add(textField);
 		textField.setColumns(10);
 		textField.setText(rec.getName());
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(102, 112, 146, 26);
 		add(textField_1);
 		textField_1.setColumns(10);
 		textField_1.setText(rec.getDate());
-		
+
 		textField_2 = new JTextField();
 		textField_2.setBounds(102, 175, 146, 26);
 		add(textField_2);
 		textField_2.setColumns(10);
 		textField_2.setText(rec.getCategory());
-		
+
 		JButton btnNewButton = new JButton("Update");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-		
+
 				String n = textField.getText();
 				String d = textField_1.getText();
 				String c = textField_2.getText();
-				Records newrec = new Records(n,d,c);
+				Records newrec = new Records(n, d, c);
 				main.getController().editPanel(index, newrec);
 			}
-		
-	});
-	
+
+		});
+
 		btnNewButton.setBounds(217, 233, 115, 29);
 		add(btnNewButton);
-		
+
 		JLabel lblName = new JLabel("Name");
 		lblName.setBounds(6, 55, 69, 20);
 		add(lblName);
-		
+
 		JLabel lblDate = new JLabel("Date");
 		lblDate.setBounds(6, 115, 69, 20);
 		add(lblDate);
-		
+
 		JLabel lblCategory = new JLabel("Category:");
 		lblCategory.setBounds(6, 178, 69, 20);
 		add(lblCategory);
-		
+
 		JButton btnBackTo = new JButton("Back to Display");
 		btnBackTo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -88,7 +89,7 @@ public class EditPanel extends JPanel{
 
 			private void showDisplayPanel() {
 				main.showDisPanel();
-				
+
 			}
 		});
 		btnBackTo.setBounds(6, 233, 160, 29);
